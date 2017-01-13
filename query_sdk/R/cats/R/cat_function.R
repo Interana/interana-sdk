16c32c17ad99acd865d00480d61a6c3d40936e92
Sachin Holla <nospamsachin@gmail.com>
2017-01-13 15:13:57 -0800

    Added a var to prove out the package can access internal vars too

--- query_sdk/R/cats/R/cat_function.R
+++ query_sdk/R/cats/R/cat_function.R
@@ -1,3 +1,6 @@
+pkg.env <- new.env()
+pkg.env$love_state <- 10
+
 #' A Cat Function
 #'
 #' This function allows you to express your love of cats.
@@ -6,12 +9,26 @@
 #' @export
 #' @examples
 #' cat_function()
- 
+
 cat_function <- function(love=TRUE){
     if(love==TRUE){
-        print("I love cats!")
+	pkg.env$love_state <- pkg.env$love_state + 1
+	print("I love cats!")
     }
     else {
+	pkg.env$love_state <- pkg.env$love_state - 1
         print("I am not a cool person.")
     }
+}
+
+#' Access Function
+#'
+#' This function allows you to check on the state of love for cats.
+#' @keywords cats
+#' @export
+#' @examples
+#' get_love_state()
+get_love_state <- function(){
+	print(paste("Love in the world is at",pkg.env$love_state," degrees"))
+	#print("foobar")
 }
