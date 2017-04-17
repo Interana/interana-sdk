6321a6acbbe04a6c54cd9a63b205531d66d49995
Sachin Holla <nospamsachin@gmail.com>
2017-04-17 13:48:28 -0700

    foobar setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = '',
+  packages = ['foobar'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A foobar lib',
+  author = 'Sachin Holla',
+  author_email = 'sachin@interana.com',
+  url = 'https://github.com/Interana/interana-sdk', # use the URL to the github repo
+  download_url = 'https://github.com/Interana/interana-sdk/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'foobar', 'example'], # arbitrary keywords
+  classifiers = [],
+)
