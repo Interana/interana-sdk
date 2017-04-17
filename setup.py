2071522e7e7e495a26640d405d623cf55c2f8c0c
Sachin Holla <nospamsachin@gmail.com>
2017-04-17 13:54:48 -0700

    fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Sachin Holla',
   author_email = 'sachin@interana.com',
   url = 'https://github.com/Interana/interana-sdk', # use the URL to the github repo
-  download_url = 'https://github.com/Interana/interana-sdk/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/Interana/interana-sdk/archive/0.2.tar.gz', # I'll explain this in a second
   keywords = ['testing', 'foobar', 'example'], # arbitrary keywords
   classifiers = [],
 )
