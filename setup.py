d3779677e725d5242b1e028eac2ff2eb8fc619a5
Sachin Holla <nospamsachin@gmail.com>
2017-04-20 11:27:14 -0700

    Change tag namespace

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
+
 setup(
   name = 'interanasdk',
-  packages = ['interanasdk'], # this must be the same as the name above
-  version = '0.2',
+  packages = ['interanasdk'],
+  version = '0.1.0',
   description = 'Interana SDK',
   scripts=['bin/ia_query_client'],
   author = 'Sachin Holla',
   author_email = 'sachin@interana.com',
-  url = 'https://github.com/Interana/interana-sdk', # use the URL to the github repo
-  download_url = 'https://github.com/Interana/interana-sdk/archive/0.2.tar.gz', # I'll explain this in a second
-  keywords = ['interana','sdk','query-api'], # arbitrary keywords
+  url = 'https://github.com/Interana/interana-sdk',
+  download_url = 'https://github.com/Interana/interana-sdk/archive/0.1.0.tar.gz',
+  keywords = [
+                 'interana',
+                 'sdk',
+                 'query-api'
+             ],
   classifiers = [],
   install_requires=[
    'requests>=2.9.1'
