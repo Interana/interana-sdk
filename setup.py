1d5eac2d0070f3ac5cb6f2a2e7ba85a34ad44bf5
Sachin Holla <nospamsachin@gmail.com>
2017-04-20 10:16:25 -0700

    Added a command line script for convenience

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
   packages = ['interanasdk'], # this must be the same as the name above
   version = '0.1',
   description = 'Interana SDK',
+  scripts=['bin/ia_query_client'],
   author = 'Sachin Holla',
   author_email = 'sachin@interana.com',
   url = 'https://github.com/Interana/interana-sdk', # use the URL to the github repo
