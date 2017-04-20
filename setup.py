9d1f930f30481aea856fac3ee96c9a850b10d79c
Sachin Holla <nospamsachin@gmail.com>
2017-04-20 10:44:29 -0700

    updated setup to use the next level tag

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'interanasdk',
   packages = ['interanasdk'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'Interana SDK',
   scripts=['bin/ia_query_client'],
   author = 'Sachin Holla',
   author_email = 'sachin@interana.com',
   url = 'https://github.com/Interana/interana-sdk', # use the URL to the github repo
-  download_url = 'https://github.com/Interana/interana-sdk/archive/0.1.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/Interana/interana-sdk/archive/0.2.tar.gz', # I'll explain this in a second
   keywords = ['interana','sdk','query-api'], # arbitrary keywords
   classifiers = [],
 )
