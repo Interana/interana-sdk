af038d251a135b3e34feb1f158ce69268796b59d
Sachin Holla <nospamsachin@gmail.com>
2017-04-20 16:20:34 -0700

    different packaging libs

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
   name = 'interanasdk',
   packages = ['interanasdk'],
-  version = '0.1.0',
+  version = '0.1.1',
   description = 'Interana SDK',
   scripts=['bin/ia_query_client'],
   author = 'Sachin Holla',
   author_email = 'sachin@interana.com',
   url = 'https://github.com/Interana/interana-sdk',
-  download_url = 'https://github.com/Interana/interana-sdk/archive/0.1.0.tar.gz',
+  download_url = 'https://github.com/Interana/interana-sdk/archive/0.1.1.tar.gz',
   keywords = [
                  'interana',
                  'sdk',
