fb8056f10d5d6e5268a93203bfe595f44aefdda2
Sachin Holla <nospamsachin@gmail.com>
2017-04-20 23:27:29 -0700

    uprev

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name = 'interanasdk',
   packages = ['interanasdk'],
-  version = '0.0.1',
+  version = '0.0.2',
   description = 'Interana SDK',
   scripts=['bin/ia_query_client'],
   author = 'Sachin Holla',
   author_email = 'sachin@interana.com',
   url = 'https://github.com/Interana/interana-sdk',
-  download_url = 'https://github.com/Interana/interana-sdk/archive/0.0.1.tar.gz',
+  download_url = 'https://github.com/Interana/interana-sdk/archive/0.0.2.tar.gz',
   keywords = [
                  'interana',
                  'sdk',
